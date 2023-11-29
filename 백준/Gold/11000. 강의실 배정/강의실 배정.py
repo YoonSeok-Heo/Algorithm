a3f408804a3482e242c5fbb99c2cbd39f35ddc87
YoonSeok-Heo <113662725+YoonSeok-Heo@users.noreply.github.com>
2023-11-29 15:05:55 +0900

    [Gold V] Title: 강의실 배정, Time: 392 ms, Memory: 64232 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,26 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+class_list = []
+
+n = int(input())
+
+for i in range(n):
+    a, b = map(int, input().split())
+    class_list.append([a, b])
+
+class_list.sort()
+
+h = []
+heapq.heappush(h, class_list[0][1])
+
+for i in range(1, n):
+    if class_list[i][0] < h[0]:
+        heapq.heappush(h, class_list[i][1])
+    else:
+        heapq.heappop(h)
+        heapq.heappush(h, class_list[i][1])
+
+print(len(h))
